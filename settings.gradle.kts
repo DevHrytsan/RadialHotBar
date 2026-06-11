585f1f7f6fac67a33172c41004d1255c9e5dbf89
DevHrytsan <3axapHrytsan@gmail.com>
2026-06-11 22:31:46 +0300

    feat: port for 26.1+ neoforge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,6 +31,9 @@ stonecutter {
 		match("1.21.1", "fabric", "neoforge")
 		match("1.21.10", "fabric", "neoforge")
 		match("1.21.11", "fabric", "neoforge")
+		match("26.1", "neoforge")
+		match("26.1.1", "neoforge")
+		match("26.1.2", "neoforge")
 
 		vcsVersion = "1.21.10-fabric"
 	}
